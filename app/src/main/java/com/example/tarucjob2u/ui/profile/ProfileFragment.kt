f74e23908a923ccbda7cb1542ba0c908f002ef4c
Kiew <kiewkiew123@hotmail.com>
2020-01-08 01:12:16 +0800

    post job

--- app/src/main/java/com/example/tarucjob2u/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/tarucjob2u/ui/profile/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.example.tarucjob2u.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.tarucjob2u.R
+import com.example.tarucjob2u.ui.post_job.job_category
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment:Fragment() {
 
@@ -15,5 +18,13 @@ class ProfileFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        button.setOnClickListener{
+            var intent = Intent(requireContext(), job_category::class.java)
+            startActivity(intent)
+        }
     }
 }
