0391642a011217b5c42191399875334691a2cd15
Jit Hong <JitHong1999@hotmail.com>
2020-01-09 23:14:42 +0800

    chonk

--- app/src/main/java/com/example/tarucjob2u/ui/report/HighPaidJobFragment.kt
+++ app/src/main/java/com/example/tarucjob2u/ui/report/HighPaidJobFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.tarucjob2u.Job
 import com.example.tarucjob2u.R
+import com.example.tarucjob2u.ui.home.JobViewModel
 
 
 class HighPaidJobFragment:Fragment() {
 
+    private lateinit var jobViewModel:JobViewModel
+    private lateinit var jobList:List<Job>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +27,9 @@ class HighPaidJobFragment:Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+//        jobViewModel = ViewModelProviders.of(this).get(JobViewModel::class.java)
+//        jobViewModel.jobList.observe(viewLifecycleOwner,
+
 
     }
 }
